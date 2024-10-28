0227b3425e38666da32bf1d3df7ba019a6e6bb0b
Tutorial92 <emailtutorial.92@gmail.com>
2024-10-28 16:13:09 +0530

    hello

--- helper/config.js
+++ helper/config.js
@@ -3,6 +3,14 @@ const config = {
   api_key: "394413329513282",
   api_secret: "RO83gimeUd0Iu5RMsfLsUtw7WYo",
   JWT_SECRET: "secretkey",
+  db: {
+    host: "cluster0.swrzh.mongodb.net",
+    port: 27017,
+    username: "emailtutorial92",
+    password: "AxgBEoL5INBowFDP",
+    name: "tutorials",
+    prefix: "linkwell_",
+  },
 };
 
 module.exports = config;
